bfc718d294f95fb8784f3de751881ca683441c5f
Kenneth Lu Diaz <ludiazken@gmail.com>
2020-11-29 13:38:35 -0500

    Tests canBeAdded and canBeMultiplied

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include "Matrix.h"
 
 int main() {
-    Matrix test(3, 4);
-    test.read("sample.txt");
+    Matrix test1(3, 4);
+    test1.read("sample.txt");
+    Matrix test2(3, 2);
+    test2.read("sample.txt");
     
 
-    std::cout << "This matrix is empty: " << test.isEmpty()
-    << std::endl;
+    // std::cout << "This matrix is empty: " << test.isEmpty()
+    // << std::endl;
+    // std::cout << "This matrix can be added: " << test1.canBeAdded(test2)
+    // << std::endl;
+    // std::cout << "This matrix can be multipied: " << test1.canBeMultiplied(test2)
+    // << std::endl;
 
 }
