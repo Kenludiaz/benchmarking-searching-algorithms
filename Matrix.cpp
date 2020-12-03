4cfe72b62056011ff16dd1d98bbdf0b65c4a6bd9
Bin <jbinwu39@gmail.com>
2020-12-03 18:34:51 +0000

    commet

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,3 +1,35 @@
+Skip to content
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+
+@jbinwu39
+Kenludiaz
+/
+sparse-matricies-linked-lists
+1
+0
+0
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+sparse-matricies-linked-lists/Matrix.cpp
+@Kenludiaz
+Kenludiaz Adds statement to create nodes in result matrix
+Latest commit 3ca8291 5 hours ago
+ History
+ 2 contributors
+@Kenludiaz@jbinwu39
+157 lines (138 sloc)  4.08 KB
+
 #include "Matrix.h"
 
 Matrix::Matrix(int rows, int cols) {
@@ -61,6 +93,7 @@ void Matrix::print() {
         std::cout << std::endl;
     }
 }
+<<<<<<< Updated upstream
 Matrix Matrix::add(Matrix& other) {
     if(!this->canBeAdded(other)) {
         std::cout << "[INVALID] These matrixes can not be added." << std::endl;
@@ -113,10 +146,75 @@ Matrix Matrix::add(Matrix& other) {
         std::cout << "========================================" << std::endl;
     }
 
+=======
+Matrix Matrix::multiply(Matrix& multiply){
+    if(!this->canBeMultiplied(multiply)){
+        std::cout << "[INVALID] These matrixes can not be multiply." << std::endl;
+        std::exit(1);
+    }
+    int counter = 0;
+    for(int i = 0; i < this->rows; i++){
+        node *temp1 = this->first;
+        node *temp2 = multiply.first;
+        for(int j = 0; j <multiply.rows; j++){
+            int value = 0;
+            while(temp1->next != nullptr || temp1->next != nullptr){
+                if(temp->rows = i && multiply.cols = j){
+                    value += this->value * multiply.value;
+                    temp1 = temp1->next;
+                    temp2 = temp2->next;
+                }
+                else{
+                    temp1 = temp1->next;
+                    temp2 = temp2->next;
+                }
+            }
+            Node* newNode = new(value, i, j);
+            node *temp1 = this->first;
+            node *temp2 = multiply.first;
+        }
+    }
+}
+
+
+Matrix Matrix::add(Matrix& other) {
+    if(!this->canBeAdded(other)) {
+        std::cout << "[INVALID] These matrixes can not be added." << std::endl;
+        std::exit(1);
+    }
+    //A copy is taken from the first matrix because every value
+    //in the resulting matrix will be equivalent until the second
+    //matrix has been iterated upon.
+    Matrix* result = new Matrix(this->rows, this->cols);
+    this->deepCopy(*(result));
+
+    Node* resultIterator = result->first;
+    Node* currentIterator = other.first;
+    while(currentIterator) {
+        if (resultIterator == nullptr) {
+            Node* newNode = new Node(currentIterator->value,
+                                     currentIterator->row,
+                                     currentIterator->col);
+            Node* previous = resultIterator->previous;
+            previous->next = newNode;
+            newNode->previous = previous;
+            resultIterator = newNode;
+            currentIterator = currentIterator->next;
+
+        } else if (currentIterator->isBehind(resultIterator)) {
+
+        } else if (currentIterator->isTied())
+    }
+
+>>>>>>> Stashed changes
     return (*result);
 }
 
 
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 bool Matrix::canBeAdded(Matrix& other){
     if (this->isEmpty() || other.isEmpty()) {
         std::cout << "[INVALID] One of these matrixes is empty." << std::endl;
@@ -127,14 +225,24 @@ bool Matrix::canBeAdded(Matrix& other){
         return false;
     }
 }
+<<<<<<< Updated upstream
+=======
+
+
+>>>>>>> Stashed changes
 void Matrix::deepCopy(Matrix& result) {
     Node* resultItr = nullptr;
     Node* currentItr = this->first;
     bool firstIteration = true;
 
     while (currentItr) {
+<<<<<<< Updated upstream
         Node* newNode = new Node(currentItr->value, 
                                  currentItr->row, 
+=======
+        Node* newNode = new Node(currentItr->value,
+                                 currentItr->row,
+>>>>>>> Stashed changes
                                  currentItr->col);
 
         if (firstIteration) {
